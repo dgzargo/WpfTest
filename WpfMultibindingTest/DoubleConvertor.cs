99a309632bc64ee259be7fcb18b71d5b519a39af
Serhii Volchetskyi <svolchetskyi@vector-software.com>
2022-05-24 12:31:00 +0300

    Initial commit

---
+++ WpfMultibindingTest/DoubleConvertor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WpfMultibindingTest;
+
+public class DoubleConvertor : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is not double doubleValue)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+
+        return doubleValue.ToString("0.########", CultureInfo.InvariantCulture);
+    }
+
+    public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is not string stringValue)
+        {
+            return null;
+        }
+        
+        var parsed = double.TryParse(stringValue, NumberStyles.Any, CultureInfo.InvariantCulture, out var doubleValue);
+        return parsed ? doubleValue : null;
+    }
+}
---
+++ WpfMultibindingTest/ViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WpfMultibindingTest;
+
+public class ViewModel : INotifyPropertyChanged
+{
+    private double? _value;
+
+    public ViewModel()
+    {
+        Value = 4;
+    }
+
+    public double? Value
+    {
+        get => _value;
+        set
+        {
+            _value = value;
+            OnPropertyChanged();
+        }
+    }
+
+    #region INotifyPropertyChanged implementation
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    #endregion
+}
